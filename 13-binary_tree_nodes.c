8066f201728cd540121f2ca692fd09cc75bb0945
root <moe_aoun@hotmail.com>
2025-02-04 19:49:17 +0000

    13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,14 +8,15 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
+    if (tree == NULL)
+        return (0);
 
-	/* If the node has at least one child, count this node */
-	if (tree->left != NULL || tree->right != NULL)
-		return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+    /* If the node has at least one child, count this node */
+    if (tree->left != NULL || tree->right != NULL)
+        return (1 + binary_tree_nodes(tree->left) +
+                binary_tree_nodes(tree->right));
 
-	/* Otherwise, it's a leaf node, don't count it */
-	return (0);
+    /* Otherwise, it's a leaf node, don't count it */
+    return (0);
 }
 
