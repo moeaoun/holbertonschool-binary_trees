19a708630eb926fb0e7b73b7b23e38fa28102239
root <moe_aoun@hotmail.com>
2025-02-04 19:13:46 +0000

    3-binary_tree_delete.c

--- binary_trees.h
+++ binary_trees.h
@@ -23,6 +23,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *tree);
 void binary_tree_delete(binary_tree_t *tree);
+void binary_tree_print(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
 
