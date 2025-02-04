40ef4701e391fbc947247fe3f7c6c7a546130aa3
root <moe_aoun@hotmail.com>
2025-02-04 18:42:07 +0000

    1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,23 +12,19 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
     binary_tree_t *new_node;
 
-    /* Check if parent is NULL */
     if (parent == NULL)
         return (NULL);
 
-    /* Create a new node */
     new_node = binary_tree_node(parent, value);
-    if (new_node == NULL) /* Check if node creation failed */
+    if (new_node == NULL)
         return (NULL);
 
-    /* If the parent already has a left child, move it to the left of the new node */
     if (parent->left != NULL)
     {
         new_node->left = parent->left;
         parent->left->parent = new_node;
     }
 
-    /* Insert the new node as the left child of the parent */
     parent->left = new_node;
 
     return (new_node);
