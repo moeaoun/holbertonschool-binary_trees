37a94b87ca8f00a2e4692429bcfb59027305f653
root <moe_aoun@hotmail.com>
2025-02-04 19:41:44 +0000

    11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,10 +8,11 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
+    if (tree == NULL)
+        return (0);
 
-	/* Size of tree is 1 (current node) + size of left subtree + size of right subtree */
-	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+    /* Size of tree is 1 (current node) + size of left subtree 
+       + size of right subtree */
+    return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
 
