57eab5a8ebdc2163dfba65e1dfa563a2860e8224
root <moe_aoun@hotmail.com>
2025-02-04 19:46:09 +0000

    12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,14 +8,14 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    /* If the node has no children, it's a leaf */
-    if (tree->left == NULL && tree->right == NULL)
-        return (1);
+	/* If the node has no children, it's a leaf */
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
 
-    /* Otherwise, count leaves in the left and right subtrees */
-    return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	/* Otherwise, count leaves in the left and right subtrees */
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
 
