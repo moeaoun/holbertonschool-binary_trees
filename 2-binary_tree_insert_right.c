41b890648532851e2624e32b4d2c83d06d331da2
root <moe_aoun@hotmail.com>
2025-02-04 18:57:53 +0000

    2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,24 +10,23 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	if (parent == NULL)
-		return (NULL);
+    binary_tree_t *new_node;
 
-	/* Create new node */
-	binary_tree_t *new_node = binary_tree_node(parent, value);
-	if (new_node == NULL)
-		return (NULL);
+    if (parent == NULL)
+        return (NULL);
 
-	/* Handle the existing right child of the parent */
-	if (parent->right != NULL)
-	{
-		new_node->right = parent->right;
-		parent->right->parent = new_node;
-	}
+    new_node = binary_tree_node(parent, value);
+    if (new_node == NULL)
+        return (NULL);
 
-	/* Insert the new node as the right child */
-	parent->right = new_node;
+    if (parent->right != NULL)
+    {
+        new_node->right = parent->right;
+        parent->right->parent = new_node;
+    }
 
-	return (new_node);
+    parent->right = new_node;
+
+    return (new_node);
 }
 
